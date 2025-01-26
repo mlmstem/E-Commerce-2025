73f0cc1827e0c34d316270086f4c4d93e544e982
LoveLive1987 <63575276+mlmstem@users.noreply.github.com>
2025-01-26 23:38:01 +1100

    after gitignore

---
+++ infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using System;
+using Core.Entities;
+using infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure.Data;
+
+public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+
+    }
+}
